e9f93e9af0e8a0dfc26a1209263b6a7e0cb20145
Zoltan Erdos <zolika84@gmail.com>
2020-06-29 12:45:28 +0100

    auth

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,8 +1,14 @@
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+import Amplify, { Auth } from 'aws-amplify';
+import awsConfig from '../aws-exports';
+Amplify.configure(awsConfig);
+
 
 const HelloPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -12,11 +18,12 @@ const HelloPage = ({ data, location }) => {
       <SEO title="Oh hello!" />
       <h1>Hello</h1>
       <p>Just wanted to say Hello!</p>
+      <AmplifySignOut />
     </Layout>
   )
 }
 
-export default HelloPage
+export default withAuthenticator(HelloPage, {})
 
 export const pageQuery = graphql`
   query {
