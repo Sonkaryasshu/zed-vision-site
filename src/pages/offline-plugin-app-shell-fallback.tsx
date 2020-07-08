ad30e9424ba092ce3dad4c0aab1fd9629d06b757
Zoltan Erdos <zoltan.erdos@me.com>
2020-07-08 15:21:48 +0100

    offline paghe

---
+++ src/pages/offline-plugin-app-shell-fallback.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="... offline" />
+      <h1>Maybe you are offline</h1>
+      <p>Check your internet connection</p>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
