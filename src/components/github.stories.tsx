30beed29ed375b26eb064b29b5cc7871fd38921f
Zoltan Erdos <zolika84@gmail.com>
2020-07-27 16:22:17 +0100

    save work

---
+++ src/components/github.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+import { GithubCommits } from "./githubCommits"
+
+export default {
+  title: "GithubCommits",
+  component: GithubCommits,
+}
+
+export const Commits = () => <GithubCommits />
