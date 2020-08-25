9f4f981301570b96f5354462a448061bf6d0fd0a
Zoltan Erdos <zoltan.erdos@me.com>
2020-08-25 17:14:06 +0100

    less rss

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -55,66 +55,7 @@ module.exports = {
         trackingId: `UA-137514306-1`,
       },
     },
-    {
-      resolve: `gatsby-plugin-feed-mdx`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMdx } }) => {
-              return allMdx.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url:
-                    site.siteMetadata.siteUrl + "/blog" + edge.node.fields.slug,
-                  guid:
-                    site.siteMetadata.siteUrl + "/blog" + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
-            },
-            query: `
-              {
-                allMdx(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields { slug }
-                      frontmatter {
-                        title
-                        date
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml",
-            title: "Your Site's RSS Feed",
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: "^/blog/",
-          },
-        ],
-      },
-    },
+
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
@@ -128,7 +69,7 @@ module.exports = {
         name: `Zed Vision - Development experience, Testing, and everything between`,
         short_name: `ZedVision`,
         description: "Blog and tech experiments",
-        start_url: `https://zed.vision`,
+        start_url: `https://www.zed.vision`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
