c71decf7761d88e3c2148b48cf8145f557f1b346
Zoltan Erdos <zoltan.erdos@me.com>
2020-08-25 16:44:27 +0100

    save up

--- gatsby/src/components/layout.tsx
+++ gatsby/src/components/layout.tsx
@@ -14,13 +14,6 @@ const components = {
 const CustomLayout: React.FC = ({ children }) => (
   <>
     <Helmet>
-      <link
-        rel="stylesheet"
-        media="none"
-        onLoad={e => (e.target.media = "all")}
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <meta
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width"
       />
