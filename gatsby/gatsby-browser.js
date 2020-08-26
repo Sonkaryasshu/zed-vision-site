a6c3a55d9169bdff3b3686793aed14c834b5015f
Zoltan Erdos <zoltan.erdos@me.com>
2020-08-26 12:19:10 +0100

    prefetch changes

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -2,33 +2,21 @@
 require("typeface-montserrat")
 require("typeface-merriweather")
 
-const { guess } = require(`guess-webpack/api`)
-
 exports.disableCorePrefetching = () => true
 
-let initialPath
-let notNavigated = true
-exports.onRouteUpdate = ({ location }) => {
-  if (initialPath !== location.pathname) {
-    notNavigated = false
-    return
-  }
-  initialPath = location.pathname
+const gatsbyBrowser = {
+  initialPath: null,
+  notNavigated: true,
 }
 
-exports.onPrefetchPathname = ({ loadPage }, pluginOptions) => {
-  if (process.env.NODE_ENV !== `production`) return
-
-  const matchedPaths = Object.keys(
-    guess({
-      path: window.location.pathname,
-      threshold: pluginOptions.minimumThreshold,
-    })
-  )
-
-  // Don't prefetch from client for the initial path as we did that
-  // during SSR
-  if (!(notNavigated && initialPath === window.location.pathname)) {
-    matchedPaths.forEach(loadPage)
+exports.onRouteUpdate = ({ location }) => {
+  if (gatsbyBrowser.initialPath !== location.pathname) {
+    gatsbyBrowser.notNavigated = false
+    return
   }
+  gatsbyBrowser.initialPath = location.pathname
+}
+
+exports.onPrefetchPathname = () => {
+  if (process.env.NODE_ENV !== `production`) return
 }
