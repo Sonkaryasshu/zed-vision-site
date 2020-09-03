c9a2c5a2a880a1cee5cef5fef23b471227ba2672
Zoltan Erdos <zoltan.erdos@me.com>
2020-09-03 12:29:23 +0100

    more cleaning up

---
+++ scripts/debts.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+if ! [ -x "$(command -v deno)" ]; then
+    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.3.2 
+fi
+
+if ! [ -x "$(command -v file_server)" ]; then
+    deno install --allow-net --allow-read https://deno.land/std@0.67.0/http/file_server.ts 
+fi
+
+if ! [ -x "$(command -v devcert)" ]; then
+    sudo npm i -g devcert-cli 
+fi
+
+if ! [ -d "./.certs" ]; then
+    mkdir .certs && cd .certs && devcert generate localhost
+fi
+
