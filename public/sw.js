020b5aaa99ce4a2d6bd13fa4f080ac016f0a6e06
Zoltan Erdos <zoltan.erdos@me.com>
2020-09-03 14:39:19 +0100

    cache the image as well

--- public/sw.js
+++ public/sw.js
@@ -100,12 +100,27 @@ registerRoute(
     url.origin === "https://fonts.gstatic.com",
   new StaleWhileRevalidate({
     cacheName: "google-fonts",
-    plugins: [new ExpirationPlugin({ maxEntries: 20 })],
+    plugins: [new ExpirationPlugin({ maxEntries: 100 })],
+  })
+)
+
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new CacheFirst({
+    cacheName: "images",
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
   })
 )
 
 registerRoute(
   ({ request }) =>
     request.destination === "script" || request.destination === "style",
-  new StaleWhileRevalidate()
+  new StaleWhileRevalidate({
+    cacheName: "static-resources",
+  })
 )
