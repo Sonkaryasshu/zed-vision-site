6c04e2600b0d4ec478322172af571385433a429e
Zoltan Erdos <zoltan.erdos@me.com>
2020-09-10 17:33:22 +0100

    render on gatsby

--- packages/video-stramer/VideoStreamer.tsx
+++ packages/video-stramer/VideoStreamer.tsx
@@ -11,7 +11,7 @@ const Video: React.FC<{ file: any }> = ({ file }) => {
   return (
     <>
       <h4>{file.name}</h4>
-      <video controls ref={videoRef} playsInline></video>
+      <video ref={videoRef} autoPlay controls loop muted playsInline></video>
     </>
   )
 }
