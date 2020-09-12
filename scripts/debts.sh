6e871aa35a0ac0d1c65535513a8574580e65d4d0
Zoltan Erdos <zoltan.erdos@me.com>
2020-09-12 12:35:51 +0100

    no more tweak

--- scripts/debts.sh
+++ scripts/debts.sh
@@ -8,11 +8,11 @@ if ! [ -x "$(command -v file_server)" ]; then
     deno install --allow-net --allow-read https://deno.land/std@0.68.0/http/file_server.ts 
 fi
 
-if ! [ -x "$(command -v devcert)" ]; then
-    sudo npm i -g devcert-cli 
-fi
+# if ! [ -x "$(command -v devcert)" ]; then
+#     # sudo npm i -g devcert-cli 
+# fi
 
 if ! [ -d "./.certs" ]; then
-    mkdir .certs && cd .certs && devcert generate localhost
+    mkdir .certs && cd .certs && nxp devcert-cli generate localhost
 fi
 
