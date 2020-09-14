e86aa98333a71a634dd093ed25da06b1ca930a6d
Zoltan Erdos <zoltan.erdos@me.com>
2020-09-14 12:43:30 +0100

    upgrade deno

--- scripts/debts.sh
+++ scripts/debts.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 if ! [ -x "$(command -v deno)" ]; then
-    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.3.3
+    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.4.0
 fi
 
 if ! [ -x "$(command -v file_server)" ]; then
-    deno install --allow-net --allow-read https://deno.land/std@0.68.0/http/file_server.ts 
+    deno install --allow-net --allow-read https://deno.land/std@0.69.0/http/file_server.ts 
 fi
 
 # if ! [ -x "$(command -v devcert)" ]; then
