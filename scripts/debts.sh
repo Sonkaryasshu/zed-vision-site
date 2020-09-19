7e012ca1da7cec08267e354c1afdd135dde1e3fd
Zed <zolika84@gmail.com>
2020-09-19 17:10:55 +0100

    deno upgrade

--- scripts/debts.sh
+++ scripts/debts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 if ! [ -x "$(command -v deno)" ]; then
-    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.4.0
+    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.4.1
 fi
 
 if ! [ -x "$(command -v file_server)" ]; then
