cf50db024655c415630bc6bc8e4d04a75efc9d56
Zoltan Erdos <zoltan.erdos@me.com>
2020-09-21 14:34:03 +0100

    client side hydrating

--- packages/gatsby/src/components/utils/testWorker.tsx
+++ packages/gatsby/src/components/utils/testWorker.tsx
@@ -1,7 +1,7 @@
 // import * as React from "react"
 // import ReactDOM from "react-dom"
 
-export const register = () => {
+export const register = (code:string) => {
   if (typeof window === "undefined") {
     return () => ({
       innerHTML: "",
@@ -37,40 +37,8 @@ export const register = () => {
 
   sha256Worker.postMessage({ id: 44, data: pastEvents });
 
-  const counter = `function Counter(props){
-    const actions = {
-      decrease: state => ({ counter: state.counter - 1 }),  
-      double: state => ({ counter: state.counter * 2 }),
-      increase: state => ({ counter: state.counter + 1 }),
-    }
-    const pastEvents = props.pastEvents || []
-    
-  
-    const [events, setEvents] = React.useState(pastEvents)
-  
-    const state = events
-      .map(ev => {
-        const text = ev.target
-        if (text.includes("-")) return "decrease"
-        else if (text.includes("+")) return "increase"
-        else if (text.includes("x2")) return "double"
-      })
-      .reduce((state, ev) => actions[ev](state), { counter: 0 })
-      
-    const onClick = e =>
-      setEvents([...events, { type: "click", target: String(e.target.innerHTML) }])
-  
-    return (
-      <div>
-        <button onClick={e => onClick(e)}>-</button>
-        <button onClick={e => onClick(e)}>x2</button>
-        Counter {props.name}:<span>{state.counter}</span>
-        <button onClick={e => onClick(e)}>+</button>
-      </div>
-    )
-  }
-`;
-  sha256Worker.postMessage({ id: 2, counter });
+ 
+  sha256Worker.postMessage({ id: 2, code });
 
   setTimeout(() => sha256Worker.postMessage({ hash: "7" }), 1000);
 
@@ -113,7 +81,7 @@ export const register = () => {
   // }
 
   worker.postMessage({
-    code: counter,
+    code: code,
     pastEvents,
   });
 
@@ -121,9 +89,9 @@ export const register = () => {
 
   const ret = {
     innerHTML: "",
-    code: counter,
+    code: code,
     compiledCode: "",
-    pastEvents: [],
+    pastEvents: pastEvents,
   };
 
   // const React = (await import("react")).default
@@ -136,7 +104,7 @@ export const register = () => {
       ret.innerHTML = d.data.domString;
 
       el.innerHTML = d.data.domString;
-      document.getElementById("zoli")?.append(el);
+      document.getElementById("zoli")?.replaceWith(el);
       // document.body.appendChild(el)
       // if (reactEl) hydrate()
     }
--- packages/gatsby/src/pages/zoli.tsx
+++ packages/gatsby/src/pages/zoli.tsx
@@ -1,5 +1,7 @@
 import Layout from "../components/layout";
 import React from "react";
+
+import ReactDOM from "react-dom";
 import { SEO } from "../components/seo";
 import { ChangeDetector } from "../components/changeDetector";
 import { graphql } from "gatsby";
@@ -17,9 +19,21 @@ interface Props {
   location: Location;
 }
 
-const Wrapper = (props: any) => (
-  <div dangerouslySetInnerHTML={{ __html: props.innerHTML }} />
-);
+const Wrapper = (props: any) => {
+  const ref = React.useRef(null);
+  
+  React.useEffect(()=>{
+    const pastEvents = props.pastEvents;
+    if (props.code){
+         const cc = new Function("props", "React", `return (${props.code})(props)`);
+         const Counter = (props: any) => cc(props, React);
+         ReactDOM.hydrate(<Counter pastEvents={pastEvents} />, ref.current);
+       
+    }
+
+  }, [props.innerHTML]);
+  return <div ref={ref} dangerouslySetInnerHTML={{ __html: props.innerHTML }} />;
+};
 
 const Comp1: React.FC<{ onEvent: (event: string) => void }> = ({ onEvent }) => {
   const [count, setCount] = React.useState(0);
@@ -55,30 +69,73 @@ const Comp1: React.FC<{ onEvent: (event: string) => void }> = ({ onEvent }) => {
   );
 };
 
-const renderedOutside = register();
+const counter = `function Counter(props){
+  const actions = {
+    decrease: state => ({ counter: state.counter - 1 }),  
+    double: state => ({ counter: state.counter * 2 }),
+    increase: state => ({ counter: state.counter + 1 }),
+  }
+  const pastEvents = props.pastEvents || []
+  
+
+  const [events, setEvents] = React.useState(pastEvents)
+
+  const state = events
+    .map(ev => {
+      const text = ev.target
+      if (text.includes("-")) return "decrease"
+      else if (text.includes("+")) return "increase"
+      else if (text.includes("x2")) return "double"
+    })
+    .reduce((state, ev) => actions[ev](state), { counter: 0 })
+    
+  const onClick = e =>
+    setEvents([...events, { type: "click", target: String(e.target.innerHTML) }])
+
+  return (
+    <div>
+      <button onClick={e => onClick(e)}>-</button>
+      <button onClick={e => onClick(e)}>x2</button>
+      CountEEer {props.name}:<span>{state.counter}</span>
+      <button onClick={e => onClick(e)}>+</button>
+    </div>
+  )
+}
+`;
+
+const renderedOutside = register(counter);
 
 const ZedZoliPage = ({ data, location }: Props) => {
   const siteTitle = data.site.siteMetadata.title;
 
-  let rr = renderedOutside();
-  const [rrrrr, setRR] = React.useState({ ...rr });
+  let workerRenderedComponent = renderedOutside();
+
+  const [renderedComponent, changeWorkerRenderedComponent] = React.useState({ ...workerRenderedComponent, outside: workerRenderedComponent  });
+  const [code, changeCode] = React.useState(counter);
+  
 
   React.useEffect(() => {
     const runner = async () => {
       const res = await sha256("ello world");
       const val = await unHash(res);
       console.log("ELLO world", val);
+
     };
     runner();
 
-    setRR({ ...rr });
-  }, [rr.innerHTML]);
+    const renderedOutside = register(code);
+    let workerRenderedComponent = renderedOutside();
+
+    changeWorkerRenderedComponent({ ...workerRenderedComponent, outside: workerRenderedComponent });
+  }, [renderedComponent.outside.innerHTML]);
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Test Worker side rendering" />
-      <h1 onClick={() => setRR({ ...rr })}>Not Found!!!</h1>
-      <Wrapper key={rrrrr.innerHTML} innerHTML={rrrrr.innerHTML}></Wrapper>
+      <textarea value={code} onChange={(e)=>changeCode(e.target.value)} >{counter}</textarea>
+      <h1 onClick={() => changeWorkerRenderedComponent({ ...workerRenderedComponent, outside: renderedComponent.outside })}>Not Found!!!</h1>
+    
+      <Wrapper key={renderedComponent.innerHTML} code={renderedComponent.compiledCode} pastEvents={renderedComponent.pastEvents} innerHTML={renderedComponent.innerHTML}></Wrapper>
       <ChangeDetector Comp1={Comp1}></ChangeDetector>
       <p>Worker side rendering</p>
       <div id="zoli"></div>
