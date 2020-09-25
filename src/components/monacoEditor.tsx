0465a851294b6853a8583ed7b598b1f107982235
Zoltan Erdos <zoltan.erdos@me.com>
2020-09-25 11:16:45 +0100

    fix

--- src/components/monacoEditor.tsx
+++ src/components/monacoEditor.tsx
@@ -7,21 +7,14 @@ import React from "react";
 import MonacoEditor from "react-monaco-editor";
 
 const MonacoEditorComp: React.FC<
-    { value: string; changeCode: (code: string) => void }
-> = ({ value, changeCode }) => {
-    return <div>
-        <MonacoEditor
+    { value: string; changeCode: (code: string) => void }> = ({ value, changeCode }) =>  <MonacoEditor
             width="100%"
             height="600"
             language="javascript"
             theme="vs-dark"
-            value={value}
-
-            options={{
-            }}
+            value = { value }
+            options={{}}
             onChange={(e) => changeCode(e)}
-        />
-    </div>;
-};
+        />;
 
 export default MonacoEditorComp;
