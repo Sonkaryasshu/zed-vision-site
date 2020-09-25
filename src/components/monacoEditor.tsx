200ee8609cf90c93cd5d4e681a0cecea30017e30
Zoltan Erdos <zoltan.erdos@me.com>
2020-09-25 15:12:20 +0100

    new build

--- src/components/monacoEditor.tsx
+++ src/components/monacoEditor.tsx
@@ -7,14 +7,16 @@ import React from "react";
 import MonacoEditor from "react-monaco-editor";
 
 const MonacoEditorComp: React.FC<
-    { value: string; changeCode: (code: string) => void }> = ({ value, changeCode }) =>  <MonacoEditor
-            width="100%"
-            height="600"
-            language="javascript"
-            theme="vs-dark"
-            value = { value }
-            options={{}}
-            onChange={(e) => changeCode(e)}
-        />;
+  { value: string; changeCode: (code: string) => void }
+> = ({ value, changeCode }) =>
+  <MonacoEditor
+    width="100%"
+    height="600"
+    language="javascript"
+    theme="vs-dark"
+    value={value}
+    options={{}}
+    onChange={(e) => changeCode(e)}
+  />;
 
 export default MonacoEditorComp;
